f5d6d50b28336bc3836072e4445b8996ba8e40cf
Frozer <simplynb14@gmail.com>
2025-05-21 13:38:35 +0530

    Add settings menu, grid size slider and display, and clear button with functionality

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+let currentSize = 16;
 
+function setUpGlobalMouseListeners(){
+    const container = document.querySelector('#container');
+
+    container.addEventListener('mousedown', (e) => {
+        e.preventDefault();
+        mouseDown = true;
+    });
+
+    document.addEventListener('mouseup', () => {
+        if (mouseDown) { // Only change state if it was previously true (i.e., user was drawing)
+            mouseDown = false;
+        }
+    });
+}
 
 function createGrid(size) {
   const container = document.querySelector('#container');
@@ -19,22 +32,50 @@ function createGrid(size) {
     square.classList.add('cell');
     container.appendChild(square);
   }
+
+  currentSize = size;
+  enableDrawing();
 }
 
 function enableDrawing(){
     const cells = document.querySelectorAll('.cell');
     cells.forEach(cell => {
-        cell.addEventListener('mousedown', () => changeColor(cell))
+        cell.addEventListener('mousedown', (e) => {
+            e.preventDefault();
+            changeColor(cell);
+        })
         cell.addEventListener('mouseover', () => {if(mouseDown) changeColor(cell)});
     })
+}
 
+function changeGridSize(){
+    const slider = document.querySelector("#grid-input");
+    const label = document.querySelector("#grid-label");
+    
+    slider.addEventListener('input', () => {
+        const value = slider.value;
+        label.textContent = `${value}x${value}`;
+    })
+
+    slider.addEventListener('change', () => {
+        const size = slider.value;
+        createGrid(size);
+    })
 }
 
 function changeColor(cell){
     cell.style.backgroundColor = 'black';
 }
 
-createGrid(16);
-enableDrawing();
+function clearButtonPress(){
+    const clearButton = document.querySelector("#clear");
+    clearButton.addEventListener('click', () => {
+        createGrid(currentSize);
+    })
+}
 
+setUpGlobalMouseListeners();
+createGrid(16);
+changeGridSize();
+clearButtonPress();
 
